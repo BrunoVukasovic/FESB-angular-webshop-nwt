3dc870218a419f4ef73f761aba5b01934b373f22
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2018-12-25 16:47:45 +0100

    IItem and item-list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
