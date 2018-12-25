a4f6216f61fe7f045370307c7b142d6d9e77abaa
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2018-12-25 22:58:22 +0100

    rating component implemented

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -9,14 +9,38 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RatingComponent implements OnInit {
   @Input() marks: number[];
   averageRating: number;
+  sum: number;
+  isActive: boolean[];
 
   constructor() { }
 
   ngOnInit() {
-    let sum = 0;
+    this.sum = 0;
+    this.isActive = [false, false, false, false, false];
+
     for (let i = 0; i < this.marks.length; i++) {
-      sum += this.marks[i];
+      this.sum += this.marks[i];
     }
-    this.averageRating = sum / this.marks.length;
+    this.averageRating = this.sum / this.marks.length;
+    this.averageRating = Math.round(this.averageRating);
+
+    for (let i = 0; i < this.averageRating; i++) {
+      this.isActive[i] = true;
+    }
+  }
+
+  onClick(clickedRating) {
+    console.log(clickedRating);
+
+    this.averageRating = (this.sum + clickedRating) / (this.marks.length + 1);
+    this.averageRating = Math.round(this.averageRating);
+    this.isActive = [false, false, false, false, false];
+
+    for (let i = 0; i < this.averageRating; i++) {
+      this.isActive[i] = true;
+    }
+
+    console.log(this.isActive);
+    console.log(this.averageRating);
   }
 }
