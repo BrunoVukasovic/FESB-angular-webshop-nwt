06b1d262582f93db7a1f53455854359464b4a7d7
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2019-01-22 18:51:37 +0100

    item-details component created

---
+++ src/app/components/item-details/item-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from 'src/app/services/item.service';
+import { IItem } from 'src/app/models/item.model';
+
+@Component({
+  selector: 'app-item-details',
+  templateUrl: './item-details.component.html',
+  styleUrls: ['./item-details.component.css']
+})
+export class ItemDetailsComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  item: IItem;
+
+  constructor( private route: ActivatedRoute, private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      this.itemService.getItems().subscribe((items: IItem[]) => {
+        this.item = items.find((selectedItem: IItem) => selectedItem.id === +params['id']);
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+}
